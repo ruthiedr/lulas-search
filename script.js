40f46ed4413497fbfdb96841f111daea536c4ace
ruthiedr <ruthieruggero@gmail.com>
2023-06-13 20:16:20 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//Code from: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_filter_list
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("searchList");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
